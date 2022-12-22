d64c0f987332845e7e7058269b918d5343fe16a0
silfredo11 <ibarrasilfredo@gmail.com>
2022-12-21 23:38:06 -0500

    I am making the result page

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const btn = document.querySelector('#button');
+const ans = document.querySelector('.answer');
 const loader = document.querySelector('.loader');
 const loaderSon1 = document.querySelector('.loader__son1');
 const loaderSon2 = document.querySelector('.loader__son2');
 const loaderSon3 = document.querySelector('.loader__son3');
-
+const main = document.querySelector('#main');
 btn.addEventListener('click',toggleAnswer);
 
 function toggleAnswer(){
+
     loader.classList.toggle('animation');
     loaderSon1.classList.toggle('animationSon');
     loaderSon2.classList.toggle('animationSon');
     loaderSon3.classList.toggle('animationSon');
+    ans.classList.toggle('visualize');
+    main.classList.toggle('display');
+    
 }
 
