288ece1ae475ad6064504197ab99e5cc00aa8110
silfredo11 <ibarrasilfredo@gmail.com>
2022-12-23 10:12:02 -0500

    I made the validacion code

--- main.js
+++ main.js
@@ -18,47 +18,54 @@ btn.addEventListener('click',toggleAnswer);
 x.addEventListener('click', removeVisualize);
 
 function toggleAnswer(){
+
     const investment =  Number(inputID.value);
     const periOfTime =  inputperiOfTime.value;
-    const interestRate =  Number(inputID.value);
-    const timeFrame =  Number(inputperiOfTime.value);
+    const interestRate =  Number(inputInterestRate.value);
+    const timeFrame =  Number(inputTimeFrame.value);
 
-    if( !investment ){
+    if(!investment || !periOfTime || !interestRate || !timeFrame){
 
-        alert('Para que la aplicación pueda funcionar, tienes que indicar el monto de tu "Depósito inicial"');
+       if(!investment){
 
-    }else if (!periOfTime) {
-        
-        alert('Para que la aplicación pueda funcionar, tienes que elegir un periodo de tiempo: "Años, Meses, Semestres, Trimestres, Meses, Semanas o Días"."');
+            inputID.classList.add('tyError');
+            
+       }
+       if(!periOfTime){
 
-    } else if (!interestRate){
+            inputperiOfTime.classList.add('tyError');
 
-        alert('Para que la aplicación pueda funcionar, tienes que insertar el porcentaje de interés.');
+       }
+       if(!interestRate){
 
-     }
-     // else if(!timeFrame){
+            inputInterestRate.classList.add('tyError');
 
-    //     alert('Para que la aplicación pueda funcionar, tienes que insertar el tiempo de la inversión. .');
+       }
+       if(!timeFrame){
 
+            inputTimeFrame.classList.add('tyError');
 
-    else{
-       loader.classList.toggle('animation');
-       loaderSon1.classList.toggle('animationSon');
-       loaderSon2.classList.toggle('animationSon');
-       loaderSon3.classList.toggle('animationSon');
-       ans.classList.toggle('visualize');
+       }
+    } else{
+        loader.classList.toggle('animation');
+        loaderSon1.classList.toggle('animationSon');
+        loaderSon2.classList.toggle('animationSon');
+        loaderSon3.classList.toggle('animationSon');
+        ans.classList.toggle('visualize');
         main.classList.toggle('display');
-    }
-    
+     }
 
    
+
     
+
 }
 
 function removeVisualize() {
 
     ans.classList.remove('visualize');
     main.classList.remove('display');
+
 }
 
 // Here start the form code
