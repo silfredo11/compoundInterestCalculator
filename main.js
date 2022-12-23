053e3d86fd083aa61566406e20c60aeb2586e142
silfredo11 <ibarrasilfredo@gmail.com>
2022-12-23 01:35:11 -0500

    I am making in the JavaScript code

--- main.js
+++ main.js
@@ -7,17 +7,51 @@ const loaderSon3 = document.querySelector('.loader__son3');
 const main = document.querySelector('#main');
 const x = document.querySelector('.header__img');
 
+// Here start the form code
+
+const inputID = document.querySelector('#InitialCapital');
+const inputperiOfTime = document.querySelector('#periOfTime');
+const inputInterestRate = document.querySelector('#interestRate');
+const inputTimeFrame = document.querySelector('#timeFrame');
+
 btn.addEventListener('click',toggleAnswer);
 x.addEventListener('click', removeVisualize);
 
 function toggleAnswer(){
+    const investment =  Number(inputID.value);
+    const periOfTime =  inputperiOfTime.value;
+    const interestRate =  Number(inputID.value);
+    const timeFrame =  Number(inputperiOfTime.value);
 
-    loader.classList.toggle('animation');
-    loaderSon1.classList.toggle('animationSon');
-    loaderSon2.classList.toggle('animationSon');
-    loaderSon3.classList.toggle('animationSon');
-    ans.classList.toggle('visualize');
-    main.classList.toggle('display');
+    if( !investment ){
+
+        alert('Para que la aplicación pueda funcionar, tienes que indicar el monto de tu "Depósito inicial"');
+
+    }else if (!periOfTime) {
+        
+        alert('Para que la aplicación pueda funcionar, tienes que elegir un periodo de tiempo: "Años, Meses, Semestres, Trimestres, Meses, Semanas o Días"."');
+
+    } else if (!interestRate){
+
+        alert('Para que la aplicación pueda funcionar, tienes que insertar el porcentaje de interés.');
+
+     }
+     // else if(!timeFrame){
+
+    //     alert('Para que la aplicación pueda funcionar, tienes que insertar el tiempo de la inversión. .');
+
+
+    else{
+       loader.classList.toggle('animation');
+       loaderSon1.classList.toggle('animationSon');
+       loaderSon2.classList.toggle('animationSon');
+       loaderSon3.classList.toggle('animationSon');
+       ans.classList.toggle('visualize');
+        main.classList.toggle('display');
+    }
+    
+
+   
     
 }
 
@@ -27,3 +61,22 @@ function removeVisualize() {
     main.classList.remove('display');
 }
 
+// Here start the form code
+
+
+
+function calculatingCI(){
+   
+
+
+ 
+}
+
+
+
+
+
+
+// const nuevoNumero = 4 * inputID.value;
+
+// console.log(`Hola mario este es el numero ${nuevoNumero}`);
