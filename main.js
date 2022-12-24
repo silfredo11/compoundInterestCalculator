87f32393fa4c823299165bd7caa7293544636d1c
silfredo11 <ibarrasilfredo@gmail.com>
2022-12-24 02:28:03 -0500

    I creade answer code in JavaScrip

--- main.js
+++ main.js
@@ -14,12 +14,20 @@ const inputperiOfTime = document.querySelector('#periOfTime');
 const inputInterestRate = document.querySelector('#interestRate');
 const inputTimeFrame = document.querySelector('#timeFrame');
 
+// Here start the form code
+
 // Error code
 
 const input1ErrorTypeMessage = document.querySelector('#errorTypeMessage_InitialCapital');
 const input2ErrorTypeMessage = document.querySelector('#errorTypeMessage_periOfTime');
 const input3ErrorTypeMessage = document.querySelector('#errorTypeMessage_interestRate');
 const input4ErrorTypeMessage = document.querySelector('#errorTypeMessage_timeFrame');
+
+// Error code
+
+// Code to make HTML from JavaScript //
+const answerP = document.querySelector('.result__p');
+// Code to make HTML from JavaScript //
 
 btn.addEventListener('click',toggleAnswer);
 x.addEventListener('click', removeVisualize);
@@ -96,13 +104,21 @@ function calculatingCI(){
      
 
      let endingBalance;
+     let noDecimals;
+     let profi; 
+     let noDecimalsProfi; 
 
      switch (timeFrame){
           case 'Y':
 
                endingBalance =  initialCapital * (1 + (interestR / 1))**(1 * numberTime);
-
+               noDecimals = endingBalance.toFixed(2);
+               profi = noDecimals - initialCapital;
+               noDecimalsProfi = profi.toFixed(2);
                
+               answerP.innerText = `El monto final después de ${numberTime} años con una tasa de interés del ${interestR * 100} % anual es de ${noDecimals}, de los cuales, ${initialCapital} pertenecen al saldo inicial y ${noDecimalsProfi} pertenecen a las ganancias generadas. `;
+
+               // El monto final después de 5 años con una tasa de interés del 5% es de 2.000.000, de los cuales, 1.000.000 pertenecen al saldo inicial, 500.000 pertenecen al dinero reinvertido y 500.000 sería la ganancia generada. 
 
           break;
 
@@ -110,7 +126,12 @@ function calculatingCI(){
 
                endingBalance =  initialCapital * (1 + (interestR / 2))**(2 * numberTime);
 
+               endingBalance =  initialCapital * (1 + (interestR / 1))**(1 * numberTime);
+               noDecimals = endingBalance.toFixed(2);
+               profi = noDecimals - initialCapital;
+               noDecimalsProfi = profi.toFixed(2);
                
+               answerP.innerText = `El monto final después de ${numberTime * 2} semestres con una tasa de interés del ${interestR * 100} % anual es de ${noDecimals}, de los cuales, ${initialCapital} pertenecen al saldo inicial y ${noDecimalsProfi} pertenecen a las ganancias generadas. `;
 
           break;
                
@@ -118,7 +139,12 @@ function calculatingCI(){
                
                endingBalance =  initialCapital * (1 + (interestR / 4))**(4 * numberTime);
 
+               endingBalance =  initialCapital * (1 + (interestR / 1))**(1 * numberTime);
+               noDecimals = endingBalance.toFixed(2);
+               profi = noDecimals - initialCapital;
+               noDecimalsProfi = profi.toFixed(2);
                
+               answerP.innerText = `El monto final después de ${numberTime * 4} trimestres con una tasa de interés del ${interestR * 100} % anual es de ${noDecimals}, de los cuales, ${initialCapital} pertenecen al saldo inicial y ${noDecimalsProfi} pertenecen a las ganancias generadas. `;
 
           break;
 
@@ -126,7 +152,12 @@ function calculatingCI(){
               
                endingBalance =  initialCapital * (1 + (interestR / 12))**(12 * numberTime);
 
+               endingBalance =  initialCapital * (1 + (interestR / 1))**(1 * numberTime);
+               noDecimals = endingBalance.toFixed(2);
+               profi = noDecimals - initialCapital;
+               noDecimalsProfi = profi.toFixed(2);
                
+               answerP.innerText = `El monto final después de ${numberTime * 12} meses con una tasa de interés del ${interestR * 100} % anual es de ${noDecimals}, de los cuales, ${initialCapital} pertenecen al saldo inicial y ${noDecimalsProfi} pertenecen a las ganancias generadas. `;
 
           break;
 
@@ -134,7 +165,12 @@ function calculatingCI(){
 
                endingBalance =  initialCapital * (1 + (interestR / 52))**(52 * numberTime);
 
+               endingBalance =  initialCapital * (1 + (interestR / 1))**(1 * numberTime);
+               noDecimals = endingBalance.toFixed(2);
+               profi = noDecimals - initialCapital;
+               noDecimalsProfi = profi.toFixed(2);
                
+               answerP.innerText = `El monto final después de ${numberTime * 52} semanas con una tasa de interés del ${interestR * 100} % anual es de ${noDecimals}, de los cuales, ${initialCapital} pertenecen al saldo inicial y ${noDecimalsProfi} pertenecen a las ganancias generadas. `;
 
           break;
 
@@ -142,7 +178,12 @@ function calculatingCI(){
 
                endingBalance =  initialCapital * (1 + (interestR / 65))**(65 * numberTime);
 
+               endingBalance =  initialCapital * (1 + (interestR / 1))**(1 * numberTime);
+               noDecimals = endingBalance.toFixed(2);
+               profi = noDecimals - initialCapital;
+               noDecimalsProfi = profi.toFixed(2);
                
+               answerP.innerText = `El monto final después de ${numberTime * 365} dias con una tasa de interés del ${interestR * 100} % anual es de ${noDecimals}, de los cuales, ${initialCapital} pertenecen al saldo inicial y ${noDecimalsProfi} pertenecen a las ganancias generadas. `;
           break;
 
           default: 
